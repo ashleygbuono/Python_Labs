930dba48b6b427f8a055c999594f45b6660133bd
Ashley Buono <ashleybuono@Ashleys-MBP.fios-router.home>
2023-09-18 20:41:31 -0400

    Ch 10 exception handler lab added

--- Ch10-ExceptionHandling/validator.py
+++ Ch10-ExceptionHandling/validator.py
@@ -1,31 +1,45 @@
-def enter_integer_in_range(prompt, low, high):
-    pass
+def enter_number_in_range(prompt, low, high, entry_function):
+    a_num = entry_function(prompt)
+    while not low <= a_num <= high:
+        print(f'{a_num} not between {low} and {high}. Try again')
+        a_num = entry_function(prompt)
+
+    return a_num
+
 
 def enter_float_in_range(prompt, low, high):
-    pass
+    return enter_number_in_range(prompt, low, high, enter_a_float)
+
+
+def enter_integer_in_range(prompt, low, high):
+    return enter_number_in_range(prompt, low, high, enter_an_integer)
+
+
+def enter_correct_type(prompt, type_function):
+    input_valid = False
+    while not input_valid:
+        try:
+            a_num = type_function(input(prompt + "==> "))
+            return a_num
+        except ValueError as ve:
+            print(f"Value entered is not an {type_function.__name__}. Try again")
+
 
 def enter_an_integer(prompt):
-
-    while True:
-        try:
-            entry = int(input(prompt))
-            return entry
-
-        except ValueError:        #this could also be TypeError, or Exception as a catch-all
-            print(f'Value entered is not an integer -- try again')
-            
+    return enter_correct_type(prompt, int)
 
 
 def enter_a_float(prompt):
-    while True:
-        try:
-            entry = float(input(prompt))
-            return entry
-
-        except ValueError:        #this could also be TypeError, or Exception as a catch-all
-            print(f'Value entered is not a float -- try again')
-            
+    return enter_correct_type(prompt, float)
 
 
-an_int = enter_a_float("Please enter a decimal number value ==> ")
-print(an_int)
+def main():
+    print(enter_integer_in_range("Enter integer between 1 and 5", 1, 5))
+    print(enter_float_in_range("Enter float between 1.5 and 5.5", 1.5, 5.5))
+
+    print(enter_an_integer('Enter integer'))
+    print(enter_a_float('Enter float'))
+
+
+if __name__ == "__main__":
+    main()
